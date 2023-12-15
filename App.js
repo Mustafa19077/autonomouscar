c97bc9f8c517b1eb13d875063dabfbd93670f737
aegurel <ahmetensarg01@gmail.com>
2023-12-15 15:32:47 +0300

    socket intialization

---
+++ App.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const WebSocket = require("ws");
+const http = require("http");
+const { send } = require("process");
+
+const DEVICES = [];
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on('connection',(ws)=>{
+    DEVICES.push(ws);
+    ws.on('message',(data)=>{
+        sendAll(data);
+    })
+    ws.on('close',(ws)=>{
+        DEVICES.splice(ws,1);
+    })
+});
+
+const PORT = 3000;
+server.listen(PORT,()=>{
+    console.log("Sunucu başladı");
+})
+
+const sendAll = (message)=>{
+    for (var i=0; i < DEVICES.length; i++) {
+        DEVICES[i].send(message); // broadcast messages to everyone including sender
+     }
+}
