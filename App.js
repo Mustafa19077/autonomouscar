e6acee41a97d6c88bbc10499bb10430ee11f3b1b
Ahmet Gürel <96393543+aegurel@users.noreply.github.com>
2023-12-15 16:49:35 +0300

    Update App.js

--- App.js
+++ App.js
@@ -21,13 +21,18 @@ wss.on('connection',(ws)=>{
     })
 });
 
-const PORT = 3000;
+const PORT = 8000;
 server.listen(PORT,()=>{
     console.log("Sunucu başladı");
 })
 
 const sendAll = (message)=>{
     for (var i=0; i < DEVICES.length; i++) {
-        DEVICES[i].send(message); // broadcast messages to everyone including sender
+        DEVICES[i].send(message);  broadcast messages to everyone including sender
      }
 }
+
+ app.get('/',(req,res)=>{
+      res.send("Merhaba burası Profff'un çöplüğü");
+     });
+})
